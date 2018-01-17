f4bc5face0fa3fb255fb72e17fe933f48baf049f
SirawichDev <sirawit0676@gmail.com>
2018-01-18 00:46:46 +0700

    gesture import

--- app/src/main/java/com/backtobasic/sirawich/gestures/MainActivity.java
+++ app/src/main/java/com/backtobasic/sirawich/gestures/MainActivity.java
@@ -2,12 +2,19 @@ package com.backtobasic.sirawich.gestures;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.support.v4.view.GestureDetectorCompat;
+import android.widget.TextView;
+import android.view.MotionEvent;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
 }
