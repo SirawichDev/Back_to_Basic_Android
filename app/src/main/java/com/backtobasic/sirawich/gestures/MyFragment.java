48712866400910f88a5622f6a98a7ee7ced67ae1
SirawichDev <sirawit0676@gmail.com>
2018-01-20 13:13:13 +0700

    add Fragement layout

---
+++ app/src/main/java/com/backtobasic/sirawich/gestures/MyFragment.java
@@ -0,0 +1,19 @@
+package com.backtobasic.sirawich.gestures;
+
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.activity_my_fragment,container,false);
+       return view;
+    }
+}
