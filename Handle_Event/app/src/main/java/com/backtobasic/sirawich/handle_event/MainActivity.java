817ed8d7cb59cf06f5001a23e523c685207ce7b0
SirawichDev <sirawit0676@gmail.com>
2018-02-08 22:08:12 +0700

    settext in onclicklistener

--- Handle_Event/app/src/main/java/com/backtobasic/sirawich/handle_event/MainActivity.java
+++ Handle_Event/app/src/main/java/com/backtobasic/sirawich/handle_event/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button bx;
@@ -15,7 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
         bx.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
-                
+                TextView xx = (TextView)findViewById(R.id.LeftText);
+                xx.setText("GGWP");
             }
         });
     }
