1fe8216252a8f111ead87a9865f2fd015b15fff3
SirawichDev <sirawit0676@gmail.com>
2018-02-08 22:06:09 +0700

    Add OnclikListener Func for button

--- Handle_Event/app/src/main/java/com/backtobasic/sirawich/handle_event/MainActivity.java
+++ Handle_Event/app/src/main/java/com/backtobasic/sirawich/handle_event/MainActivity.java
@@ -2,12 +2,21 @@ package com.backtobasic.sirawich.handle_event;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button bx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bx = (Button) findViewById(R.id.button);
+
+        bx.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                
+            }
+        });
     }
 }
