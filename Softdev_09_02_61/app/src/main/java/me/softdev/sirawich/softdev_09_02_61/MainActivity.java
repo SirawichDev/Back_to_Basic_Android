a592d2f397f635b972eb0c4194e922998513328f
Ex <sirawit0676@gmail.com>
2018-02-09 11:50:46 +0700

    finished to day

--- Softdev_09_02_61/app/src/main/java/me/softdev/sirawich/softdev_09_02_61/MainActivity.java
+++ Softdev_09_02_61/app/src/main/java/me/softdev/sirawich/softdev_09_02_61/MainActivity.java
@@ -6,11 +6,31 @@ import android.widget.*;
 import android.widget.Button;
 import android.view.View;
 public class MainActivity extends AppCompatActivity {
-
+    private Button bx;
+    private Button Long;
+    private EditText ed;
+    private TextView tx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Long = (Button) findViewById(R.id.btn1);
+        Long.setOnLongClickListener(new Button.OnLongClickListener(){
+
+            @Override
+            public boolean onLongClick(View v) {
+                tx = (TextView)findViewById(R.id.Fuck);
+                tx.setText("MIEW MIEW");
+                return true;
+            }
+
+
+        });
+    }
+
+    public void Showme(View view){
+        tx = (TextView)findViewById(R.id.Fuck);
+        tx.setText(" ");
 
     }
 
