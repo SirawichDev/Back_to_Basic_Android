39494d1e18777c289adbb8a0501fca6996f77503
SirawichDev <sirawit0676@gmail.com>
2018-02-10 20:08:15 +0700

    finished clear function for two converter

--- SoftDev_HomeWorks/09_02_61/app/src/main/java/homework/softdev/sirawich/softdev_homework_1/MainActivity.java
+++ SoftDev_HomeWorks/09_02_61/app/src/main/java/homework/softdev/sirawich/softdev_homework_1/MainActivity.java
@@ -20,81 +20,102 @@ public class MainActivity extends AppCompatActivity {
     private EditText Edt1;
     private EditText Edt2;
     private TextView status;
+    final boolean[] sets = {true};
+    int i =0;
     private static final String TAG = "HELLO inp1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.e(TAG,"First");
         setContentView(R.layout.activity_main);
+
         btn1 = (Button) findViewById(R.id.btn1);
         btn1.setOnClickListener(new Button.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Log.e(TAG,"Here is onClick btn1");
-                c2finput();
+            public void onClick(View v) {
+                    Log.e(TAG, "Here is onClick btn1");
+                    c2finput();
+
             }
         });
         btn2 = (Button)findViewById(R.id.btn2);
         Log.e(TAG,"test");
         btn2.setOnClickListener(new Button.OnClickListener(){
 
-            public void onClick(View view){
-                Log.e(TAG,"Here is onClick btn2");
-                f2cinput();
+            public void onClick(View view) {
+
+                    Log.e(TAG, "Here is onClick btn2");
+                    f2cinput();
             }
         });
     }
 
     private void c2finput() {
+
         if (!input1()) {
             Log.e(TAG,"Go to inp1");
+            btn1.setText("Convert !");
             return;
         }
-        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
+
+            btn1.setText("Clear !");
+
     }
+
     private void f2cinput() {
         if (!input2()) {
             Log.e(TAG,"Go to inp2");
             return;
-
         }
-        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
+        btn2.setText("Clear !");
     }
-
     private boolean input1() {
-        String regexStr = "^[0-9]*$";
-        Log.e(TAG,"Hello inp1");
+        String regexStr = "^[0-9]*$*";
+        String GG = "°F";
+        Log.e(TAG, "Hello inp1");
         Edt1 = (EditText) findViewById(R.id.Editext1);
-        while (!Edt1.getText().toString().trim().matches(regexStr)) {
-            Toast.makeText(getApplicationContext(), "Sorry you must Input Only The number!!", Toast.LENGTH_LONG).show();
-            Edt1.setText("");
 
+
+        if (!Edt1.getText().toString().trim().matches(regexStr)) {
+            if(Edt1.getText().toString().contains("°F")){
+                Edt1.setText("");
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "Sorry you must Input Only The number!!", Toast.LENGTH_LONG).show();
+                Edt1.setText("");
+            }
+          return  false;
         }
         if (Edt1.getText().toString().isEmpty()) {
             Toast.makeText(getApplicationContext(), "Sorry Can't Empty ,Please Enter The number", Toast.LENGTH_LONG).show();
             return false;
-
         }
-        int cal1 = Integer.parseInt(Edt1.getText().toString());
-        int conver1= (cal1 * 9/5)+32;
-        Edt1.setText(String.valueOf(conver1)+"°F");
-        Log.e(TAG,"Go to true");
-    return true;
-    }
 
+        int cal1 = Integer.parseInt(Edt1.getText().toString());
+        int convert1= (cal1 * 9/5)+32;
+        Edt1.setText(String.valueOf(convert1)+"°F");
+        Log.e(TAG,"Go to true");
+        return true;
+    }
     private boolean input2() {
         String regexStr = "^[0-9]*$";
         Edt2 = (EditText) findViewById(R.id.Editext2);
         if (!Edt2.getText().toString().trim().matches(regexStr)) {
+            if(Edt2.getText().toString().contains("°F")){
+                Edt2.setText("");
+            }
             Toast.makeText(getApplicationContext(), "Sorry you must Input Only The number!!", Toast.LENGTH_LONG).show();
             Edt2.setText("");
             return false;
-
         }
         if (Edt2.getText().toString().isEmpty()) {
             Toast.makeText(getApplicationContext(), "Sorry Can't Empty ,Please Enter The number", Toast.LENGTH_LONG).show();
             return false;
         }
+
+        int cal2 = Integer.parseInt(Edt2.getText().toString());
+        int convert2 = (cal2-32) * 5/9;
+        Edt2.setText(String.valueOf(convert2)+"°C");
     return true;
     }
 }
