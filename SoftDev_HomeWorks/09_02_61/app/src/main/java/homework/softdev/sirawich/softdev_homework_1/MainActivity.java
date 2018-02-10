cd7e74041160eb863f5ce0cc5744ee5e51b15e63
SirawichDev <sirawit0676@gmail.com>
2018-02-10 11:22:02 +0700

    aligh layout finished

---
+++ SoftDev_HomeWorks/09_02_61/app/src/main/java/homework/softdev/sirawich/softdev_homework_1/MainActivity.java
@@ -0,0 +1,13 @@
+package homework.softdev.sirawich.softdev_homework_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
