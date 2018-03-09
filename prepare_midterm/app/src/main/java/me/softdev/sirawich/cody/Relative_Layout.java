7ea3ed999edbcad0c468c69e96e3d15bd6758bfb
SirawichDev <sirawit0676@gmail.com>
2018-03-10 02:34:11 +0700

    added button in Relative Layout

---
+++ prepare_midterm/app/src/main/java/me/softdev/sirawich/cody/Relative_Layout.java
@@ -0,0 +1,13 @@
+package me.softdev.sirawich.cody;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Relative_Layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative__layout);
+    }
+}
