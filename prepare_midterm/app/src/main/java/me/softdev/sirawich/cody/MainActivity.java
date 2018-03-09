3ac3cdcb91ad79603b237eff269991d798204ffa
SirawichDev <sirawit0676@gmail.com>
2018-03-09 09:57:42 +0700

    added event click for Checkbox 1

--- prepare_midterm/app/src/main/java/me/softdev/sirawich/cody/MainActivity.java
+++ prepare_midterm/app/src/main/java/me/softdev/sirawich/cody/MainActivity.java
@@ -2,12 +2,28 @@ package me.softdev.sirawich.cody;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    CheckBox cb1,cb2;
+    private static final String TAG = "From CheckBox 1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cb1 = (CheckBox)findViewById(R.id.ck1);
+        cb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b == true) {
+                    Log.i(TAG, "onCheckedChanged: Click me ");
+                }
+                else
+                    Log.i(TAG, "onCheckedChanged: UnClick me ");
+            }
+        });
+
     }
 }
