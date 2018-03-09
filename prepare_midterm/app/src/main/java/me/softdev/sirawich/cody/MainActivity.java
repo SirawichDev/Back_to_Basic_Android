ab1ba9e2384fc899ba0584f3bd86afcb8c75fac2
SirawichDev <sirawit0676@gmail.com>
2018-03-09 10:04:06 +0700

    Make Toast For Checkbox1

--- prepare_midterm/app/src/main/java/me/softdev/sirawich/cody/MainActivity.java
+++ prepare_midterm/app/src/main/java/me/softdev/sirawich/cody/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox cb1,cb2;
+    Toast t1,t2;
     private static final String TAG = "From CheckBox 1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +21,13 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(b == true) {
                     Log.i(TAG, "onCheckedChanged: Click me ");
+                    t1 = Toast.makeText(getApplicationContext(),"Clicked",Toast.LENGTH_SHORT);
+                    t1.show();
                 }
                 else
                     Log.i(TAG, "onCheckedChanged: UnClick me ");
+                t1 = Toast.makeText(getApplicationContext(),"UnClicked",Toast.LENGTH_SHORT);
+                t1.show();
             }
         });
 
