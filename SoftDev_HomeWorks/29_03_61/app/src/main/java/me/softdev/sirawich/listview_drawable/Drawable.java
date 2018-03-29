30429839d37dd44a9bdc9812376d7e24d4b20f49
SirawichDev <sirawit0676@gmail.com>
2018-03-29 20:44:15 +0700

    finished drawer menu with toggle actionbar

---
+++ SoftDev_HomeWorks/29_03_61/app/src/main/java/me/softdev/sirawich/listview_drawable/Drawable.java
@@ -0,0 +1,35 @@
+package me.softdev.sirawich.listview_drawable;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class Drawable extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable);
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawableLayout);
+        mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.opent,R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
