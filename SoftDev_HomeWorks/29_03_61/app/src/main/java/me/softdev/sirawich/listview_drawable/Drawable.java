cf64a99f65ec021fdf419e97d1f7738c622dacfd
SirawichDev <sirawit0676@gmail.com>
2018-03-29 21:47:16 +0700

    hidding noactionbar when open drawer menu

--- SoftDev_HomeWorks/29_03_61/app/src/main/java/me/softdev/sirawich/listview_drawable/Drawable.java
+++ SoftDev_HomeWorks/29_03_61/app/src/main/java/me/softdev/sirawich/listview_drawable/Drawable.java
@@ -1,20 +1,24 @@
 package me.softdev.sirawich.listview_drawable;
-
+import android.support.v7.widget.Toolbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+
 public class Drawable extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private android.support.v7.widget.Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawable);
 
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawableLayout);
         mToggle = new ActionBarDrawerToggle(this,mDrawerLayout,R.string.opent,R.string.close);
 
